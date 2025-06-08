7f263797e151a8e94cf1bc8ef00daa041c2d3adf
8uandj <144869577+8uandj@users.noreply.github.com>
2025-06-08 12:33:00 +0700

    3rd

--- backend/routes/announcements.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const announcementController = require('../controllers/announcementController');
-
-router.get('/', announcementController.getAll);
-router.post('/', announcementController.create);
-
-module.exports = router;
--- backend/routes/tickets.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ticketController = require('../controllers/ticketController');
-
-router.post('/', ticketController.bookTicket);
-router.put('/:id/cancel', ticketController.cancelTicket);
-router.get('/track/:code', ticketController.getTicketByCode);
-
-module.exports = router;
